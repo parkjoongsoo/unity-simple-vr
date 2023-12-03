7978fea64f5988cee57a89f9143c6fdbaa95ef6c
parkjoongsoo <152037023+parkjoongsoo@users.noreply.github.com>
2023-12-03 19:42:25 +0900

    다른 사람 만듦

---
+++ Manager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+    public void LoadScene(int sceneId)
+    {
+        SceneManager.LoadScene(sceneId);
+    }
+}
